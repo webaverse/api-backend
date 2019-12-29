18239ed39a013bc0897d2e9b4e3cbd8d395737d8
Chris <chris.eddy.dev@gmail.com>
2019-12-29 05:29:42 -0800

    test endpoints and url code, cleanup params

--- blockchainEvents.js
+++ blockchainEvents.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 const { discordBot } = require('./discordBot.js')
 const createKeccakHash = require('keccak')
 const BN = require('bn.js');
-const btoa = require('btoa')
+const fetch = require("node-fetch")
 
 const main = async () => {
 
@@ -13,22 +13,63 @@ const main = async () => {
     let filter = null
     let botOnline = false
 
-    ws.addEventListener('open', function (event) {
+    const testUrl = (url) => {
+        return new Promise(async (accept, reject) => {
+            const response = await fetch(url, {
+                method: "GET"
+            })
+            if(response.ok){
+                accept(`\`\`\`yaml\n${url} is online, status code: ${response.status}\`\`\``) 
+            }
+            else{
+                accept(`\`\`\`diff\n-${url} is down with status code: ${response.status}\`\`\``) 
+            }
+        })
+    }
 
-        discordClient.on('message', msg => {
+    const endpoints = [
+        "https://dev.exokit.org/",
+        "https://root.exokit.org/",
+        "https://web.exokit.org/",
+        "https://exokit.org/",
+        "https://linux.exokit.org/",
+        "https://store.exokit.org/",
+        "https://sync.exokit.org/",
+        "https://payments.exokit.org/",
+        "https://content.exokit.org/",
+        "https://docs.exokit.org/",
+        "https://git.exokit.org/",
+        "https://login.exokit.org/",
+        "https://upload.exokit.org/",
+        "https://grid.exokit.org/",
+        "https://multplayer.exokit.org/",
+        "https://tokens.exokit.org/",
+        "https://presence.exokit.org/",
+        "https://editor.exokit.org/",
+        "https://browser.exokit.org/",
+        "https://land.exokit.org/",
+        "https://spatial-engine.exokit.org/",
+        "https://meshing.exokit.org/"
+    ]
+
+    ws.addEventListener('open', (event) => {
+
+        discordClient.on('message', async (msg) => {
             if (msg.content.slice(0, 1) === '!') {
 
                 let functionHash = null;
-                let param = null;
                 let ethCall
 
+                const command = msg.content.slice(1).split(", ")[0]
+                let param = msg.content.slice(1).split(", ")[1]
+                console.log(param)
 
-                switch(msg.content.slice(1).split(",")[0]){
+                switch(command){
                     case "getId()":
                         if(botOnline){
                             functionHash = `0x${createKeccakHash('keccak256').update('getId(uint256)').digest('hex').slice(0, 8)}`
-                            param = new BN(msg.content.slice(1).split(", ")[1].slice(2), 16).toString(16, 256 * 2)
-                            msg.reply(`Calling ${msg.content.slice(1).split(", ")[0]} with: ${msg.content.slice(1).split(", ")[1]}`);
+                            param = new BN(param.slice(2), 16).toString(16, 256 * 2)
+                            msg.reply(`Calling ${command} with: ${param}`);
                             ethCall = functionHash + param
                             ws.send(JSON.stringify({"jsonrpc":"2.0","method":"eth_call","params": [{"to": address, "data": ethCall}, "latest"],"id":1}))
                         }
@@ -36,21 +77,21 @@ const main = async () => {
                     case "uri()":
                         if(botOnline){
                             functionHash = `0x${createKeccakHash('keccak256').update('uri(uint256)').digest('hex').slice(0, 8)}`
-                            param = new BN(msg.content.slice(1).split(", ")[1], 16).toString(16, 256 * 2)
-                            msg.reply(`Calling ${msg.content.slice(1).split(", ")[0]} with: ${msg.content.slice(1).split(", ")[1]}`);
+                            param = new BN(param, 16).toString(16, 256 * 2)
+                            msg.reply(`Calling ${command} with: ${param}`);
                             ethCall = functionHash + param
                             ws.send(JSON.stringify({"jsonrpc":"2.0","method":"eth_call","params": [{"to": address, "data": ethCall}, "latest"],"id":1}))
                         }
                         break;
                     case "getTx()":
                         if(botOnline){
-                            param = msg.content.slice(1).split(", ")[1]
-                            msg.reply(`Calling ${msg.content.slice(1).split(", ")[0]} with: ${msg.content.slice(1).split(", ")[1]}`);
+                            param = param
+                            msg.reply(`Calling ${command} with: ${param}`);
                             ws.send(JSON.stringify({"jsonrpc":"2.0","method":"eth_getTransactionByHash","params": [param],"id":1}))
                         } 
                         break;
                     case "help":
-                        discordChannel.send(`\`\`\`<FUNCTION_NAME()>, <PARAM> \n Blockchain Functions: getTx(uint256 hash), getId(uint256 hash), uri(uint256 id) \n Start bot: !start \n Stop bot: !stop\`\`\``)
+                        discordChannel.send(`\`\`\`!<FUNCTION_NAME()>, <PARAM>\n Blockchain Functions: getTx(uint256 hash), getId(uint256 hash), uri(uint256 id)\n Other Functions: testUrl(), testEndpoints()\n Start bot: !start \n Stop bot: !stop \`\`\``)
                         break;
                     case "start":
                         botOnline = true;
@@ -61,7 +102,18 @@ const main = async () => {
                         botOnline = false;
                         discordChannel.send(`Blockchain Bot is Offline!. I'm going to sleep now, \`!start\` to wake me up. zzz \`!help\` for more info`)
                         break;
+                    case "testUrl()":
+                        const result = await testUrl(param)
+                        msg.reply(result)
+                        break;
+                    case "testEndpoints()":
+                        endpoints.forEach(async (url) => {
+                            let status = await testUrl(url)
+                            msg.reply(status)
+                        })
+                        break;
                     default:
+                        discordChannel.send(`hmmm I do not recognize that command... \`!help\` for more info`)
                         break;
                 }
 
