fb7060efb7b609e557fcc2dddff9c4cc9b821e20
Chris <chris.eddy.dev@gmail.com>
2020-07-28 00:42:41 -0700

    learning things

--- ipfsMigration.js
+++ ipfsMigration.js
@@ -1,21 +1,20 @@
-const IPFS = require('ipfs')
-const { BufferList } = require('bl')
 const { putObject } = require('./aws.js')
+const fetch = require('node-fetch');
 
 const main = async () => {
-    const node = await IPFS.create()
-    for await (const fileInfo of node.files.ls('/')) {
-        console.log(fileInfo)
-        for await (const file of ipfs.get(fileInfo.cid)) {
-            console.log(file.path)
-            if (!file.content) continue;
-            const content = new BufferList()
-            for await (const chunk of file.content) {
-              content.append(chunk)
-            }
-            const response = await putObject('ipfs.exokit.org', file.name, content)
-            console.log(response)
-          }
-    }
+    const listRes = await fetch('https://packages.exokit.org/')
+    const list = await listRes.json()
+    list.forEach(async (package) => {
+        const detailsRes = await fetch(`https://packages.exokit.org/${package}`)
+        details = detailsRes.json()
+        const webBundle = await fetch(`https://ipfs.exokit.org/ipfs/${details.dataHash}`)
+        console.log(webBundle)
+        // upload to s3
+        details.icons.forEach((icon) => {
+            const preview = await fetch(`https://ipfs.exokit.org/ipfs/${icon.hash}`)
+            console.log(preview)
+            // upload to s3
+        })
+    })
 }
 main()
