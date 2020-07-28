da4992258076a571174db9a80d9575edb38481e9
Chris <chris.eddy.dev@gmail.com>
2020-07-28 16:25:59 -0700

    fix Buffer usage bug

--- ipfsMigration.js
+++ ipfsMigration.js
@@ -10,7 +10,7 @@ const main = async () => {
             const detailsRes = await fetch(`https://packages.exokit.org/${package}`)
             const details = await detailsRes.json()
             const webBundle = await fetch(`https://ipfs.exokit.org/ipfs/${details.dataHash}`)
-            await putObject('ipfs.exokit.org', `${details.dataHash}.wbn`, await webBundle.text())
+            await putObject('ipfs.exokit.org', `${details.dataHash}.wbn`, new Buffer.from(await webBundle.arrayBuffer()))
             details.icons.forEach(async (icon) => {
                 let ext = mime.getExtension(icon.type)
                 if (!ext && (icon.type === 'model/gltf-binary+preview' || icon.type === 'model/gltf-binary')) {
@@ -18,7 +18,7 @@ const main = async () => {
                 }
                 if (ext) {
                     const preview = await fetch(`https://ipfs.exokit.org/ipfs/${icon.hash}`)
-                    await putObject('ipfs.exokit.org', `${icon.hash}.${ext}`, await preview.text())
+                    await putObject('ipfs.exokit.org', `${icon.hash}.${ext}`, new Buffer.from(await preview.arrayBuffer()))
                 }
                 else {
                     console.log('File rejected, MIME type not accepted.', icon.type)
