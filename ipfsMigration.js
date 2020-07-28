a81d55e408f94f20166740373552a6527113802c
Chris <chris.eddy.dev@gmail.com>
2020-07-28 15:34:20 -0700

    finish the migration script

--- ipfsMigration.js
+++ ipfsMigration.js
@@ -3,17 +3,31 @@ const fetch = require('node-fetch');
 const mime = require('mime');
 
 const main = async () => {
-    const listRes = await fetch('https://packages.exokit.org/')
-    const list = await listRes.json()
-    list.forEach(async (package) => {
-        const detailsRes = await fetch(`https://packages.exokit.org/${package}`)
-        const details = await detailsRes.json()
-        const webBundle = await fetch(`https://ipfs.exokit.org/ipfs/${details.dataHash}`)
-        const s3BundleRes = await putObject('ipfs.exokit.org', `${details.dataHash}.wbn`, webBundle.toString())
-        details.icons.forEach(async (icon) => {
-            const preview = await fetch(`https://ipfs.exokit.org/ipfs/${icon.hash}`)
-            const s3IconRes = await putObject('ipfs.exokit.org', `${icon.hash}.${mime.getExtension(icon.type)}`, preview.toString())
+    try {
+        const listRes = await fetch('https://packages.exokit.org/')
+        const list = await listRes.json()
+        list.forEach(async (package) => {
+            const detailsRes = await fetch(`https://packages.exokit.org/${package}`)
+            const details = await detailsRes.json()
+            const webBundle = await fetch(`https://ipfs.exokit.org/ipfs/${details.dataHash}`)
+            await putObject('ipfs.exokit.org', `${details.dataHash}.wbn`, await webBundle.text())
+            details.icons.forEach(async (icon) => {
+                let ext = mime.getExtension(icon.type)
+                if (!ext && (icon.type === 'model/gltf-binary+preview' || icon.type === 'model/gltf-binary')) {
+                    ext = 'glb'
+                }
+                if (ext) {
+                    const preview = await fetch(`https://ipfs.exokit.org/ipfs/${icon.hash}`)
+                    await putObject('ipfs.exokit.org', `${icon.hash}.${ext}`, await preview.text())
+                }
+                else {
+                    console.log('File rejected, MIME type not accepted.', icon.type)
+                }
+            })
         })
-    })
+    }
+    catch (e) {
+        console.log(e)
+    }
 }
 main()
