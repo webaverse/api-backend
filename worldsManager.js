5d0ec03af012e3b12e15393bca41cd7d93b5a4e0
Chris <chris.eddy.dev@gmail.com>
2020-08-11 20:29:06 -0700

    major work, active world manager, fix publicDNS, refactor functions

--- worldsManager.js
+++ worldsManager.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const Client = require('ssh2').Client;
 const fs = require('fs');
 const { accessKeyId, secretAccessKey } = require('./config.json');
+const { wordlists } = require('./bip39');
 const awsConfig = new AWS.Config({
     credentials: new AWS.Credentials({
         accessKeyId,
@@ -15,72 +16,64 @@ const EC2 = new AWS.EC2(awsConfig);
 const worldMap = new Map();
 
 // searchs through all of our ec2 instances and makes Map of worlds with their unique name as the key.
-const fetchWorldList = () => {
-    const describeParams = {
-        Filters: [
-            {
-                Name: "tag:Purpose",
-                Values: [
-                    "world"
-                ]
-            }
-        ]
-    };
-    EC2.describeInstances(describeParams, (error, data) => {
-        if (!error && data.Reservations.length > 0) {
-            data.Reservations[0].Instances.forEach(instance => {
-                instance.Tags.forEach(tag => {
-                    if (tag.Value.split('-')[0] === 'world') {
-                        worldMap.set(tag.Value, instance)
-                    }
+const getWorldList = () => {
+    return new Promise((resolve, reject) => {
+        const describeParams = {
+            Filters: [
+                {
+                    Name: "tag:Purpose",
+                    Values: ["world"]
+                }
+            ]
+        };
+        EC2.describeInstances(describeParams, (error, data) => {
+            if (!error && data.Reservations.length > 0) {
+                data.Reservations.forEach(r => {
+                    worldMap.set(r.Instances[0].Tags[0].Value, r.Instances[0])
+                    resolve(worldMap)
                 })
-            })
-            // console.log(worldMap)
-        }
-        else {
-            console.error(error)
-        }
+            }
+            else {
+                console.error(error)
+                reject()
+            }
+        })
     })
 }
 
-fetchWorldList();
-
-// Routes API requests for interfacing with worlds.
-const _handleWorldsRequest = (req, res) => {
-    try {
-        const { method } = req;
-        // Create a new ec2 instance, SSH copy the worldSrc into new instance, return host and port.
-        if (method === 'POST') {
-            const uuid = uuidv4();
-            const instanceParams = {
-                ImageId: 'ami-0cd230f950c3de5d8',
-                InstanceType: 't2.nano',
-                KeyName: 'Exokit',
-                MinCount: 1,
-                MaxCount: 1,
-                SecurityGroupIds: [
-                    "sg-0c41f4cc265915ed7"
-                ],
-                TagSpecifications: [
-                    {
-                        ResourceType: "instance",
-                        Tags: [
-                            {
-                                Key: "Name",
-                                Value: "world-" + uuid
-                            },
-                            {
-                                Key: "Purpose",
-                                Value: "world"
-                            }
-                        ]
-                    }
-                ]
-            };
-            EC2.runInstances(instanceParams, (error, data) => {
-                if (!error) {
-                    // console.log('New World Instance:', data);
-                    worldMap.set('world-' + uuid, data.Instances[0])
+const createNewWorld = () => {
+    return new Promise((resolve, reject) => {
+        const uuid = uuidv4();
+        const instanceParams = {
+            ImageId: 'ami-0cd230f950c3de5d8',
+            InstanceType: 't2.nano',
+            KeyName: 'Exokit',
+            MinCount: 1,
+            MaxCount: 1,
+            SecurityGroupIds: [
+                "sg-0c41f4cc265915ed7"
+            ],
+            TagSpecifications: [
+                {
+                    ResourceType: "instance",
+                    Tags: [
+                        {
+                            Key: "Name",
+                            Value: "world-" + uuid
+                        },
+                        {
+                            Key: "Purpose",
+                            Value: "world"
+                        }
+                    ]
+                }
+            ]
+        };
+        EC2.runInstances(instanceParams, (error, data) => {
+            if (!error) {
+                setTimeout(async () => {
+                    await getWorldList();
+                    const newInstance = worldMap.get('world-' + uuid);
                     const conn = new Client();
                     conn.on('ready', () => {
                         conn.sftp((error, sftp) => {
@@ -88,36 +81,56 @@ const _handleWorldsRequest = (req, res) => {
                                 sftp.fastPut('worldSrc/package.json', '/home/ubuntu/package.json', (error) => {
                                     if (error) {
                                         console.error(error)
+                                        reject(error)
                                     }
                                 })
                             }
                             else {
                                 console.error(error)
+                                reject(error)
                             }
                         })
-                        const newWorld = {
-                            worldName: 'world-' + uuid,
-                            launchTime: data.Instances[0].LaunchTime,
-                        }
-                        res.statusCode = 200;
-                        res.end(JSON.stringify(newWorld));
+                        resolve({
+                            name: 'world-' + uuid,
+                            host: newInstance.PublicDnsName,
+                            launchTime: newInstance.LaunchTime,
+                        }) 
                     }).connect({
-                        // todo: this DNS is missing for some reason in the instance callback, need to fetch public DNS after
-                        host: data.Instances[0].PublicDnsName,
+                        host: newInstance.PublicDnsName,
                         port: 22,
                         username: 'ubuntu',
                         privateKey: fs.readFileSync('keys/server.pem')
                     });
-                }
-                else {
-                    console.error(error)
-                    res.statusCode = 500;
-                    res.end()
-                }
-            })
+                }, 30000)
+            }
+            else {
+                console.error(error)
+                reject(error)
+            }
+        })
+    })
+}
+
+// Routes API requests for interfacing with worlds.
+const _handleWorldsRequest = async (req, res) => {
+    try {
+        const { method } = req;
+        // Create a new ec2 instance, SSH copy the worldSrc into new instance, return host and port.
+        if (method === 'POST') {
+            const newWorld = await createNewWorld();
+            if (newWorld) {
+                console.log('New World created:', newWorld.name)
+                res.statusCode = 200;
+                res.end(JSON.stringify(newWorld));
+            }
+            else {
+                res.statusCode = 500;
+                res.end();
+            }
         }
         else if (method === 'GET') {
-
+            res.statusCode = 200;
+            res.end(JSON.stringify({ message: 'lol' }));
         }
         else if (method === 'DELETE') {
 
@@ -131,6 +144,20 @@ const _handleWorldsRequest = (req, res) => {
         console.log(e);
     }
 }
+
+const worldsManager = async () => {
+    await getWorldList()
+    const activeWorlds = new Map()
+    worldMap.forEach(world => {
+        // running or pending
+        if (world.State.Code === 16 || world.State.Code === 0) {
+            activeWorlds.set(world.Tags[1].Value, world)
+        }
+    })
+    console.log(`World Manager Online! ${activeWorlds.size} active World(s)`)
+}
+
+worldsManager()
 
 module.exports = {
     _handleWorldsRequest
