48e3a051e7e1a1f759a27fad58347a00554a4fd3
Chris <chris.eddy.dev@gmail.com>
2020-08-19 00:34:57 -0700

    fix NVM post-install again

--- installWorld.sh
+++ installWorld.sh
@@ -9,9 +9,9 @@ scp -o StrictHostKeyChecking=no -i keys/server.pem world-server/world-server.zip
 
 ssh -o StrictHostKeyChecking=no -i keys/server.pem -t ubuntu@$PUBLIC_DNS << EOF
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash 
-    export NVM_DIR="$([ -z "\${XDG_CONFIG_HOME-}" ]
-    [ -s "\$NVM_DIR/nvm.sh" ]
-    \. "\$NVM_DIR/nvm.sh"
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
     nvm install 14
     nvm use 14
     cd world-server/
