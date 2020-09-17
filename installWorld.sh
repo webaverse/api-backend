2affaba273236830ff765f7d265efc8dcd07267f
Chris <chris.eddy.dev@gmail.com>
2020-09-16 22:47:24 -0700

    remove certs/ upload from server

--- installWorld.sh
+++ installWorld.sh
@@ -2,12 +2,12 @@
 
 PUBLIC_IP=$1
 
-zip -ur ./world-server/world-server.zip ./certs/
+zip -ur ./world-server/world-server.zip
 
 scp -o StrictHostKeyChecking=no -i /tmp/keys/server.pem world-server/world-server.zip ubuntu@$PUBLIC_IP:~
 
 ssh -o StrictHostKeyChecking=no -i /tmp/keys/server.pem ubuntu@$PUBLIC_IP << EOF
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash 
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
     export NVM_DIR="\$HOME/.nvm"
     [ -s "\$NVM_DIR/nvm.sh" ]
     \. "\$NVM_DIR/nvm.sh"
@@ -24,9 +24,8 @@ ssh -o StrictHostKeyChecking=no -i /tmp/keys/server.pem ubuntu@$PUBLIC_IP << EOF
     sudo apt-get install unzip -y
     unzip world-server.zip
     npm i forever -g
-    mkdir node_modules/dialog/certs/ 
-    cp -r certs/ node_modules/dialog/
-    cd node_modules/dialog/ 
+    mkdir node_modules/dialog/certs/
+    cd node_modules/dialog/
     sudo MEDIASOUP_LISTEN_IP=${PUBLIC_IP} MEDIASOUP_ANNOUNCED_IP=${PUBLIC_IP} 'DEBUG=${DEBUG:=*mediasoup* *INFO* *WARN* *ERROR*}' 'INTERACTIVE=${INTERACTIVE:='false'}' $(which node) $(which forever) start index.js
     exit
 EOF
