a82e8308421312960f141ac83a6a2dd89c9c82ea
Chris <chris.eddy.dev@gmail.com>
2020-09-17 08:39:38 -0700

    redo install script, install node.

--- installWorld.sh
+++ installWorld.sh
@@ -2,28 +2,18 @@
 
 PUBLIC_IP=$1
 
-scp -o StrictHostKeyChecking=no -i /tmp/keys/server.pem world-server/world-server.zip ubuntu@$PUBLIC_IP:~
+scp -o StrictHostKeyChecking=no -i /tmp/keys/server.pem ./world-server.zip ubuntu@$PUBLIC_IP:~
 
 ssh -o StrictHostKeyChecking=no -i /tmp/keys/server.pem ubuntu@$PUBLIC_IP << EOF
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-    export NVM_DIR="\$HOME/.nvm"
-    [ -s "\$NVM_DIR/nvm.sh" ]
-    \. "\$NVM_DIR/nvm.sh"
-    [ -s "\$NVM_DIR/bash_completion" ]
-    \. "\$NVM_DIR/bash_completion"
-    nvm install 14
-    nvm use 14
-    cd ~/world-server/
-    sudo apt-get dist-upgrade -y
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+    sudo setcap 'cap_net_bind_service=+ep' /usr/bin/node
     sudo apt-get update -y
-    sudo apt-get install build-essential -y
-    sudo apt-get install python -y
-    sudo apt-get install python3 -y
     sudo apt-get install unzip -y
     unzip world-server.zip
-    npm i forever -g
+    sudo npm i forever -g
     mkdir node_modules/dialog/certs/
-    cd node_modules/dialog/
-    sudo MEDIASOUP_LISTEN_IP=${PUBLIC_IP} MEDIASOUP_ANNOUNCED_IP=${PUBLIC_IP} 'DEBUG=${DEBUG:=*mediasoup* *INFO* *WARN* *ERROR*}' 'INTERACTIVE=${INTERACTIVE:='false'}' $(which node) $(which forever) start index.js
+    cd ~/node_modules/dialog/
+    MEDIASOUP_LISTEN_IP=${PUBLIC_IP} MEDIASOUP_ANNOUNCED_IP=${PUBLIC_IP} DEBUG=\${DEBUG:='*mediasoup* *INFO* *WARN* *ERROR*'} INTERACTIVE=\${INTERACTIVE:='false'} forever start index.js
     exit
 EOF
--- routes/worlds.js
+++ routes/worlds.js
@@ -390,22 +390,28 @@ const determineWorldBuffer = (worldMap) => {
     })
 }
 
-const updateZipFile = async () => {
-    if (!fs.existsSync('world-server/world-server.zip')) {
-        console.log('Fetching world-server ZIP release...');
-        const response = await fetch('https://github.com/webaverse/world-server/releases/download/254383237/world-server.zip');
-        if (response.ok) {
-            console.log('Writing ZIP to local file on server...');
-            await streamPipeline(response.body, fs.createWriteStream('./world-server/world-server.zip'))
-            console.log('ZIP written to server successfully!');
+const updateZipFile = () => {
+    return new Promise(async (resolve, reject) => {
+        if (!fs.existsSync('./world-server.zip')) {
+            console.log('Fetching world-server ZIP release...');
+            const response = await fetch('https://github.com/webaverse/world-server/releases/download/258806584/world-server.zip');
+            if (response.ok) {
+                console.log('Writing ZIP to local file on server...');
+                await streamPipeline(response.body, fs.createWriteStream('./world-server.zip'));
+                console.log('ZIP written to server successfully!');
+                resolve();
+            } else {
+                console.error('couldnt pull ZIP for some reason:', response);
+                reject();
+            }
         } else {
-            console.error('couldnt pull ZIP for some reason:', response)
+            resolve();
         }
-    }
+    })
 }
 
-const main = () => {
-    updateZipFile()
+const main = async () => {
+    await updateZipFile();
     worldsManager();
 }
 main()
