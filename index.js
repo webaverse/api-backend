2f5c917aa99ffd9cfa63eaa2b89d6c73986aba9c
Avaer Kazmer <a@modules.io>
2020-09-19 19:09:10 -0400

    Switch to new blockchain code

--- index.js
+++ index.js
@@ -22,15 +22,7 @@ const namegen = require('./namegen.js');
 const Base64Encoder = require('./encoder.js').Encoder;
 // const {JSONServer, CustomEvent} = require('./dist/sync-server.js');
 const {SHA3} = require('sha3');
-const bip39 = require('bip39');
-const flowJs = require('./flow.js');
-const flow = {
-  sdk: require('@onflow/sdk'),
-  types: require('@onflow/types'),
-  crypto: flowJs.crypto,
-  signingFunction: flowJs.signingFunction,
-};
-const flowConstants = require('./flow-constants.js');
+const blockchain = require('./blockchain.js');
 const config = require('./config.json');
 const {accessKeyId, secretAccessKey, /*githubUsername, githubApiKey,*/ githubPagesDomain, githubClientId, githubClientSecret, stripeClientId, stripeClientSecret} = config;
 const awsConfig = new AWS.Config({
@@ -62,6 +54,7 @@ const Discord = require('discord.js');
 // const ethUtil = require('./ethereumjs-util.js');
 const api = require('./api.js');
 const { _handleStorageRequest } = require('./routes/storage.js');
+const { _handleAccountsRequest } = require('./routes/accounts.js');
 const { _handleWorldsRequest, _startWorldsRoute } = require('./routes/worlds.js');
 
 const CERT = fs.readFileSync('./certs/fullchain.pem');
@@ -119,82 +112,7 @@ function _getKeyFromBindingUrl(u) {
     return [];
   }
 }
-const _genKeys = async mnemonic => {
-  const seed = await bip39.mnemonicToSeed(mnemonic);
-  return flow.crypto.genKeys({
-    entropy: seed.toString('hex'),
-    entropyEnc: 'hex',
-  });
-};
-const _isSealed = tx => tx.status >= 4;
-const _waitForTx = async txid => {
-  for (;;) {
-    const response2 = await flow.sdk.send(await flow.sdk.pipe(await flow.sdk.build([
-      flow.sdk.getTransactionStatus(txid),
-    ]), [
-      flow.sdk.resolve([
-        flow.sdk.resolveParams,
-      ]),
-    ]), { node: flowConstants.host });
-    // console.log('got response 2', response2);
-    if (_isSealed(response2.transaction)) {
-      return response2;
-    } else {
-      await new Promise((accept, reject) => {
-        setTimeout(accept, 500);
-      });
-    }
-  }
-};
-const _createAccount = async mnemonic => {
-  const userKeys = await _genKeys(mnemonic);
 
-  const acctResponse = await flow.sdk.send(await flow.sdk.pipe(await flow.sdk.build([
-    flow.sdk.getAccount(config.address),
-  ]), [
-    flow.sdk.resolve([
-      flow.sdk.resolveParams,
-    ]),
-  ]), { node: flowConstants.host });
-  const seqNum = acctResponse.account.keys[0].sequenceNumber;
-
-  const signingFunction = flow.signingFunction.signingFunction(config.privateKey);
-
-  const response = await flow.sdk.send(await flow.sdk.pipe(await flow.sdk.build([
-    flow.sdk.authorizations([flow.sdk.authorization(config.address, signingFunction, 0)]),
-    flow.sdk.payer(flow.sdk.authorization(config.address, signingFunction, 0)),
-    flow.sdk.proposer(flow.sdk.authorization(config.address, signingFunction, 0, seqNum)),
-    flow.sdk.limit(100),
-    flow.sdk.transaction`
-      transaction(publicKeys: [String]) {
-        prepare(signer: AuthAccount) {
-          let acct = AuthAccount(payer: signer)
-          for key in publicKeys {
-            acct.addPublicKey(key.decodeHex())
-          }
-        }
-      }
-    `,
-    flow.sdk.args([
-      // flow.sdk.arg(2, t.Uint8),
-      flow.sdk.arg([userKeys.flowKey], flow.types.Array(flow.types.String)),
-    ]),
-  ]), [
-    flow.sdk.resolve([
-      flow.sdk.resolveArguments,
-      flow.sdk.resolveParams,
-      flow.sdk.resolveAccounts,
-      flow.sdk.resolveRefBlockId({ node: flowConstants.host }),
-      flow.sdk.resolveSignatures,
-    ]),
-  ]), { node: flowConstants.host });
-  // console.log('got response 4', response);
-  const response2 = await _waitForTx(response.transactionId);
-  // console.log('got response 5', response2);
-  const address = response2.transaction.events[0].payload.value.fields[0].value.value.slice(2);
-  // console.log('got response 6', userKeys.address);
-  return address;
-};
 const _makePromise = () => {
   let accept, reject;
   const p = new Promise((a, r) => {
@@ -299,10 +217,11 @@ try {
                 name = namegen(2).join('-');
               }
               if (!mnemonic) {
-                mnemonic = bip39.entropyToMnemonic(crypto.randomBytes(32));
+                mnemonic = blockchain.makeMnemonic();
               }
               if (!addr) {
-                addr = await _createAccount(mnemonic);
+                const userKeys = await blockchain.genKeys(mnemonic);
+                addr = await blockchain.createAccount(userKeys);
               }
               if (!state) {
                 state = _randomString();
