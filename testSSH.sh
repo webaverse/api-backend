b75ced6fd64b18ab8fc26a052d8c87d1110f61d5
Chris <chris.eddy.dev@gmail.com>
2020-10-09 23:44:33 -0700

    change test SSH

--- testSSH.sh
+++ testSSH.sh
@@ -2,7 +2,7 @@
 
 PUBLIC_IP=$1
 
-ssh -o StrictHostKeyChecking=no -i /tmp/keys/server.pem ubuntu@$PUBLIC_IP << EOF
+ssh -o StrictHostKeyChecking=no -i ./keys/server.pem ubuntu@$PUBLIC_IP << EOF
     echo all good
     exit
 EOF
