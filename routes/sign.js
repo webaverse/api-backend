baa4495c36c3f9ec27b0bb7ebd92721ea9aa4b7a
Avaer Kazmer <a@modules.io>
2020-10-22 05:21:56 -0400

    Update sign.js

--- routes/sign.js
+++ routes/sign.js
@@ -93,13 +93,14 @@ const _handleSignRequest = async (req, res) => {
                               // const proxyContract = contracts[chainName][proxyContractName];
                               const proxyContractAddress = addresses[chainName][proxyContractName];
                               
-                              const {returnValues: {from, to, tokenId, value}} = log;
+                              const {returnValues} = log;
+                              const {from, to} = returnValues;
                               if (to === proxyContractAddress) {
                                 // signable
                                 if (contractName === 'FT') {
                                   const amount = {
                                     t: 'uint256',
-                                    v: new web3[chainName].utils.BN(value),
+                                    v: new web3[chainName].utils.BN(returnValues.value),
                                   };
                                   const timestamp = {
                                     t: 'uint256',
@@ -111,8 +112,8 @@ const _handleSignRequest = async (req, res) => {
                                   };
                                   const message = web3[chainName].utils.encodePacked(from, amount, timestamp, chainId);
                                   const hashedMessage = web3[chainName].utils.sha3(message);
-                                  const sgn = web3[chainName].eth.accounts.sign(hashedMessage, wallet.getPrivateKeyString()); // await web3[chainName].eth.personal.sign(hashedMessage, address);
-                                  console.log('signed', sgn);
+                                  const sgn = web3[chainName].eth.accounts.sign(hashedMessage, wallet.getPrivateKeyString());
+                                  // console.log('signed', sgn);
                                   const {r, s, v} = sgn;
                                   /* const r = sgn.slice(0, 66);
                                   const s = '0x' + sgn.slice(66, 130);
@@ -123,13 +124,62 @@ const _handleSignRequest = async (req, res) => {
                                     to: from,
                                     amount: '0x' + web3[chainName].utils.padLeft(amount.v.toString(16), 32),
                                     timestamp: timestamp.v,
-                                    chainId: amount.v.toNumber(),
+                                    chainId: chainId.v.toNumber(),
+                                    r,
+                                    s,
+                                    v,
+                                  }));
+                                } else if (contractName === 'NFT') {
+                                  const tokenId = {
+                                    t: 'uint256',
+                                    v: new web3[chainName].utils.BN(returnValues.tokenId),
+                                  };
+                                  
+                                  const hashSpec = await contracts[chainName][contractName].methods.getHash(tokenId.v).call();
+                                  const hash = {
+                                    t: 'uint256',
+                                    v: new web3[chainName].utils.BN(hashSpec),
+                                  };
+                                  const filenameSpec = await contracts[chainName][contractName].methods.getMetadata(hashSpec, 'filename').call();
+                                  const filename = {
+                                    t: 'string',
+                                    v: filenameSpec,
+                                  };
+                                  console.log('got filename hash', hash, filename);
+
+                                  // get sidechain deposit receipt signature
+                                  const timestamp = {
+                                    t: 'uint256',
+                                    v: txid,
+                                  };
+                                  const chainId = {
+                                    t: 'uint256',
+                                    v: new web3[chainName].utils.BN(chainIds[chainName][proxyContractName]),
+                                  };
+
+                                  const filenameHash = web3[chainName].utils.sha3(filename.v);
+                                  // console.log('sign', {tokenId: log.tokenId, hashSpec, from, tokenId, hash, filenameHash, timestamp, chainId});
+                                  const message = web3[chainName].utils.encodePacked(from, tokenId, hash, filenameHash, timestamp, chainId);
+                                  const hashedMessage = web3[chainName].utils.sha3(message);
+                                  const sgn = web3[chainName].eth.accounts.sign(hashedMessage, wallet.getPrivateKeyString()); // await web3.eth.personal.sign(hashedMessage, address);
+                                  const {r, s, v} = sgn;
+                                  /* const r = sgn.slice(0, 66);
+                                  const s = '0x' + sgn.slice(66, 130);
+                                  const v = '0x' + sgn.slice(130, 132); */
+                                  // console.log('got', JSON.stringify({r, s, v}, null, 2));
+                                  res.end(JSON.stringify({
+                                    to: from,
+                                    tokenId: '0x' + web3[chainName].utils.padLeft(tokenId.v.toString(16), 32),
+                                    hash: '0x' + web3[chainName].utils.padLeft(hash.v.toString(16), 32),
+                                    filenameHash,
+                                    timestamp: timestamp.v,
+                                    chainId: chainId.v.toNumber(),
                                     r,
                                     s,
                                     v,
                                   }));
                                 } else {
-                                  res.end(JSON.stringify(log));
+                                  res.end(JSON.stringify(null));
                                 }
                               } else {
                                 res.end(JSON.stringify(null));
