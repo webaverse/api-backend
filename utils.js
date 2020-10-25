2af151c55e58337651bce821fde75d6df908162f
Avaer Kazmer <a@modules.io>
2020-10-25 08:21:16 -0400

    Add getExt util

--- utils.js
+++ utils.js
@@ -4,6 +4,11 @@ const _setCorsHeaders = res => {
     res.setHeader('Access-Control-Allow-Methods', '*');
     return res;
 };
+function getExt(fileName) {
+  const match = fileName.match(/\.([^\.]+)$/);
+  return match && match[1].toLowerCase();
+}
 module.exports = {
-    _setCorsHeaders
+  _setCorsHeaders,
+  getExt,
 }
