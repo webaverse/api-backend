f4a96e007d22dfacc37afeac366d0785f61d8430
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-18 17:19:36 -0600

    cleanup and use https syntax

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,18 +1,7 @@
 const url = require('url');
 const { _setCorsHeaders } = require('../utils.js');
 
-/* const _jsonParse = s => {
-   try {
-       return JSON.parse(s);
-   } catch(err) {
-       return null;
-   }
-}; */
-
 const _handleAnalyticsRequest = async (req, res) => {
-    const request = url.parse(req.url);
-    const path = request.path.split('/')[1];
-
     const {
       contentId,
       ownerAddress,
@@ -25,7 +14,6 @@ const _handleAnalyticsRequest = async (req, res) => {
       assetScale
     } = req.body
 
-    let match;
     try {
         res = _setCorsHeaders(res);
         const {method} = req;
@@ -34,30 +22,48 @@ const _handleAnalyticsRequest = async (req, res) => {
         } else if (method === 'GET') {
             res.end();
         } else if (method === 'POST') {
-          const params = {
-            TableName: monetization,
-            Item: {
-              contentId,
-              ownerAddress,
-              monetizationPointer,
-              amount,
-              assetCode,
-              assetScale,
-              timestamp: Date.now()
-            }
-          };
-        
-          ddbd.put(params, function(err, data) {
-              if (err) {
-                  console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-              } else {
-                  console.log("Added item:", JSON.stringify(data, null, 2));
+          const bs = [];
+          req.on('data', d => {
+              bs.push(d);
+          });
+          req.on('end', async () => {
+              try {
+                const b = Buffer.concat(bs);
+                const s = b.toString('utf8');
+                const json = JSON.parse(s);
+
+                const {
+                  amount,
+                  assetCode,
+                  assetScale
+                } = json
+
+                const params = {
+                  TableName: monetization,
+                  Item: {
+                    contentId,
+                    ownerAddress,
+                    monetizationPointer,
+                    amount,
+                    assetCode,
+                    assetScale,
+                    timestamp: Date.now()
+                  }
+                };
+              
+                ddbd.put(params, function(err, data) {
+                    if (err) {
+                        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                    } else {
+                        console.log("Added item:", JSON.stringify(data, null, 2));
+                    }
+                });
+             } catch (err) {
+                console.log(err);
+                res.statusCode = 500;
+                res.end(err.stack);
               }
           });
-        } else {
-          res.statusCode = 404;
-          res.end();
-        }
     } catch (err) {
         console.log(err);
         res.statusCode = 500;
