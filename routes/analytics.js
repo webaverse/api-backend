e3c6aa51f7962d0d37a74a97c1d0370fe4f010f8
Avaer Kazmer <a@modules.io>
2020-12-18 19:21:32 -0500

    Bugfix analytics.js

--- routes/analytics.js
+++ routes/analytics.js
@@ -22,61 +22,60 @@ const _handleAnalyticsRequest = async (req, res) => {
     const monetizationPointer = path[2];
 
     try {
-        res = _setCorsHeaders(res);
-        const {method} = req;
-        if (method === 'OPTIONS') {
-            res.end();
-        } else if (method === 'GET') {
-            res.end();
-        } else if (method === 'POST') {
-          const bs = [];
-          req.on('data', d => {
-              bs.push(d);
-          });
-          req.on('end', async () => {
-              try {
-                const b = Buffer.concat(bs);
-                const s = b.toString('utf8');
-                const json = JSON.parse(s);
+      res = _setCorsHeaders(res);
+      const {method} = req;
+      if (method === 'OPTIONS') {
+          res.end();
+      } else if (method === 'GET') {
+          res.end();
+      } else if (method === 'POST') {
+        const bs = [];
+        req.on('data', d => {
+            bs.push(d);
+        });
+        req.on('end', async () => {
+            try {
+              const b = Buffer.concat(bs);
+              const s = b.toString('utf8');
+              const json = JSON.parse(s);
 
-                const {
+              const {
+                amount,
+                assetCode,
+                assetScale
+              } = json
+
+              const params = {
+                TableName: "monetization",
+                Item: {
+                  contentId,
+                  eventId: uuid.v1(),
+                  ownerAddress,
+                  monetizationPointer,
                   amount,
                   assetCode,
-                  assetScale
-                } = json
-
-                const params = {
-                  TableName: "monetization",
-                  Item: {
-                    contentId,
-                    eventId: uuid.v1(),
-                    ownerAddress,
-                    monetizationPointer,
-                    amount,
-                    assetCode,
-                    assetScale,
-                    timestamp: Date.now()
+                  assetScale,
+                  timestamp: Date.now()
+                }
+              };
+            
+              ddbd.put(params, function(err, data) {
+                  if (err) {
+                      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                  } else {
+                      console.log("Added item:", JSON.stringify(data, null, 2));
                   }
-                };
-              
-                ddbd.put(params, function(err, data) {
-                    if (err) {
-                        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-                    } else {
-                        console.log("Added item:", JSON.stringify(data, null, 2));
-                    }
-                });
-             } catch (err) {
-                console.log(err);
-                res.statusCode = 500;
-                res.end(err.stack);
-              }
-          });
-    } catch (err) {
-        console.log(err);
-        res.statusCode = 500;
-        res.end(err.stack);
-    }
+              });
+           } catch (err) {
+              console.log(err);
+              res.statusCode = 500;
+              res.end(err.stack);
+            }
+        });
+      } else {
+        res.statusCode = 404;
+        res.end('not found');
+      }
 }
 
 module.exports = {
