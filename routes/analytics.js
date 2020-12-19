73e7adbfe5319f67255f1cbc4b329a124934dc5e
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2020-12-18 18:29:50 -0600

    add res.end

--- routes/analytics.js
+++ routes/analytics.js
@@ -62,8 +62,10 @@ const _handleAnalyticsRequest = async (req, res) => {
               ddbd.put(params, function(err, data) {
                   if (err) {
                       console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                      res.end(JSON.stringify(err, null, 2));
                   } else {
                       console.log("Added item:", JSON.stringify(data, null, 2));
+                      res.end(JSON.stringify(data, null, 2));
                   }
               });
            } catch (err) {
