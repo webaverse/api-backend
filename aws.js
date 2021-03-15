87c859e8a4c11e436ee19712006a9d8b50de9c99
soulofmischief <soulofmischief@protonmail.com>
2021-03-15 05:43:01 -0500

    Add DynamoDB put/get calls

--- aws.js
+++ aws.js
@@ -10,6 +10,18 @@ const awsConfig = new AWS.Config({
 });
 const s3 = new AWS.S3(awsConfig);
 
+const ddb = new AWS.DynamoDB({
+  ...awsConfig,
+  apiVersion: '2012-08-10',
+});
+
+const ddbd = new AWS.DynamoDB.DocumentClient({
+  ...awsConfig,
+  apiVersion: '2012-08-10',
+});
+
+const defaultDynamoTable = 'sidechain-cache';
+
 const getObject = (bucket, key) => {
     return new Promise(async (resolve, reject) => {
         const params = { Bucket: bucket, Key: key };
@@ -41,6 +53,34 @@ const putObject = (bucket, key, data, type) => {
     })
 }
 
+async function getDynamoItem(id, TableName = defaultDynamoTable) {
+  const params = {
+    TableName,
+    Key: {id: String(id)}
+  }
+
+  try {
+    return await ddbd.get(params).promise();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+async function putDynamoItem(id, data, TableName = defaultDynamoTable) {
+  const params = {
+    TableName,
+    Item: {...data, id: String(id)},
+  };
+
+  try {
+    return ddbd.put(params).promise();
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
 function uploadFromStream(bucket, key, type) {
   const pass = new stream.PassThrough();
   const params = {Bucket: bucket, Key: key, Body: pass, ACL: 'public-read'};
@@ -59,6 +99,10 @@ function uploadFromStream(bucket, key, type) {
 }
 
 module.exports = {
+  ddb,
+  ddbd,
+  getDynamoItem,
+  putDynamoItem,
   getObject,
   putObject,
   uploadFromStream,
