cc2cdd8d61d606d5d46817a7190d7c525012a0da
Avaer Kazmer <a@modules.io>
2021-03-30 03:26:05 -0400

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,31 @@
+const accountKeys = [
+  'name',
+  'avatarId',
+  'avatarName',
+  'avatarExt',
+  'avatarPreview',
+  'loadout',
+  'homeSpaceId',
+  'homeSpaceName',
+  'homeSpaceExt',
+  'homeSpacePreview',
+  'ftu',
+  'mainnetAddress',
+];
+const ids = {
+  lastCachedBlockAccount: 'lastCachedBlock',
+  lastCachedBlockNft: -1,
+};
+const tableNames = {
+  mainnetAccount: 'mainnet-cache-account',
+  mainnetNft: 'mainnet-cache-nft',
+  mainnetsidechainAccount: 'sidechain-cache-account',
+  mainnetsidechainNft: 'sidechain-cache-nft',
+};
+const polygonVigilKey = `0937c004ab133135c86586b55ca212a6c9ecd224`; // XXX should go into config
+module.exports = {
+  accountKeys,
+  ids,
+  tableNames,
+  polygonVigilKey,
+};
