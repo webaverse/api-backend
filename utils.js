f2c5c8fbb49afecae01aa9bfff0c304ebe2dbd0a
Avaer Kazmer <a@modules.io>
2021-04-27 04:03:31 -0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -9,6 +9,7 @@ const _setCorsHeaders = res => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Headers', '*');
     return res;
 };
 function getExt(fileName) {
