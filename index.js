e1b0cdfc61ebfc7318dc3ad81ed04d474d252fb8
shawticus <shawmakesmagic@gmail.com>
2021-05-09 18:49:04 -0700

    Integrate API and change port to use env var

--- index.js
+++ index.js
@@ -19,12 +19,17 @@ const {NetworkNames, getBlockchain} = require('./blockchain.js');
 const {accountKeys, ids, nftIndexName, redisPrefixes, mainnetSignatureMessage} = require('./constants.js');
 const {connect: redisConnect, getRedisClient} = require('./redis');
 const ethereumJsUtil = require('./ethereumjs-util.js');
-const {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, CACHE_HOST_URL} = require('./config.js');
+const {GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET, CACHE_HOST_URL, HTTP_PORT, HTTPS_PORT} = require('./config.js');
 const {getChainToken, getChainOwnerNft} = require("./tokens.js");
 const {worldManager, _handleWorldsRequest} = require('./routes/worlds.js');
 const {_handleSignRequest} = require('./routes/sign.js');
 const {_handleUnlockRequest, _handleLockRequest, _handleDecryptRequest, _isCollaborator, _isSingleCollaborator} = require('./routes/unlock.js');
 const {_handleAnalyticsRequest} = require('./routes/analytics.js');
+
+// api docs deps
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const {addV1Routes} = require("./api/v1/index.js");
 
 let CERT = null;
 let PRIVKEY = null;
@@ -2181,13 +2186,28 @@ const _ws = protocol => (req, socket, head) => {
   }
 };
 
-const server = http.createServer(_req('http:'));
-server.on('upgrade', _ws('http:'));
-const server2 = https.createServer({
-  cert: CERT,
-  key: PRIVKEY,
-}, _req('https:'));
-server2.on('upgrade', _ws('https:'));
+
+
+
+
+
+const _makeApp = protocol => {
+  const app = express();
+  app.use(fileUpload());
+  addV1Routes(app);
+  app.all('*', _req(protocol));
+  return app;
+};
+const server = http.createServer(_makeApp('http:'));
+server.on("upgrade", _ws("http:"));
+const server2 = https.createServer(
+  {
+    cert: CERT,
+    key: PRIVKEY,
+  },
+  _makeApp('https:')
+);
+server2.on("upgrade", _ws("https:"));
 
 const _warn = err => {
   console.warn('uncaught: ' + err.stack);
@@ -2195,10 +2215,10 @@ const _warn = err => {
 process.on('uncaughtException', _warn);
 process.on('unhandledRejection', _warn);
 
-server.listen(PORT);
-server2.listen(443);
+server.listen(HTTP_PORT);
+server2.listen(HTTPS_PORT);
 
-console.log(`http://127.0.0.1:${PORT}`);
-console.log(`https://127.0.0.1:443`);
+console.log(`http://127.0.0.1:${HTTP_PORT}`);
+console.log(`https://127.0.0.1:${HTTPS_PORT}`);
 
 })();
