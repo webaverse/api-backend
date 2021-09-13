28f7cc248125e65b2f976d7426fc78b719bc05ce
Avaer Kazmer <a@modules.io>
2021-09-13 11:15:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ const {connect: redisConnect, getRedisClient} = require('./redis');
 const ethereumJsUtil = require('./ethereumjs-util.js');
 const gotNfts = require('got-nfts');
 const OpenAI = require('openai-api');
+const GPT3Encoder = require('gpt-3-encoder');
 
 let config = require('fs').existsSync('./config.json') ? require('./config.json') : null;
 
@@ -77,7 +78,85 @@ const ses = new AWS.SES(new AWS.Config({
 // const eventsManager = require('./events-manager.js');
 
 const Discord = require('discord.js');
+
+const getAiPrefix = (() => {
+  let aiPrefix = null;
+  let aiPrefixLoad = null;
+  return async () => {
+    if (aiPrefix) {
+      return aiPrefix;
+    } else {
+      if (!aiPrefixLoad) {
+        aiPrefixLoad = (async () => {]
+          const res = await fetch(`https://webaverse.github.io/app/ai/ai-prefix.js`);
+          const j = await res.text();
+          aiPrefixLoad = null;
+          setTimeout(() => {
+            aiPrefix = null;
+          }, 10 * 60 * 1000);
+        })();
+      }
+      return await aiPrefixLoad;
+    }
+  };
+})();
+
+OpenAI.prototype._send_request = (sendRequest => async function(url, method, opts = {}) {
+  let camelToUnderscore = (key) => {
+    let result = key.replace(/([A-Z])/g, " $1");
+    return result.split(' ').join('_').toLowerCase();
+  }
+  
+  // console.log('got req', url, method, opts);
+
+  const data = {};
+  for (const key in opts) {
+    data[camelToUnderscore(key)] = opts[key];
+  }
+
+  const rs = await new Promise((accept, reject) => {
+    const req = https.request(url, {
+      method,
+      headers: {
+        'Authorization': `Bearer ${this._api_key}`,
+        'Content-Type': 'application/json'
+      }
+    }, res => {
+      res.setEncoding('utf8');
+      accept(res);
+    });
+    req.end(Object.keys(data).length ? JSON.stringify(data) : '');
+    req.on('error', reject);
+  });
+  return rs;
+})(OpenAI.prototype._send_request);
 const openai = new OpenAI(config.openAiKey);
+const _openAiCodex = async (prompt, stop) => {
+  const maxTokens = 4096;
+  const max_tokens = maxTokens - GPT3Encoder.encode(prompt).length;
+  console.log('max tokens: ' + max_tokens);
+  const gptRes = await openai.complete({
+    engine: 'davinci-codex',
+    prompt,
+    stop,
+    temperature: 0,
+    topP: 1,
+    max_tokens,
+    stream: true,
+
+    /* stream: false,
+    prompt: o.prompt, // 'this is a test',
+    maxTokens: o.maxTokens, // 5,
+    temperature: o.temperature, // 0.9,
+    topP: o.topP, // 1,
+    presencePenalty: o.presencePenalty, // 0,
+    frequencyPenalty: o.frequencyPenalty, // 0,
+    bestOf: o.bestOf, // 1,
+    n: o.n, // 1,
+    stop: o.stop, // ['\n'] */
+  });
+  return gptRes;
+};
 
 // const api = require('./api.js');
 // const { _handleStorageRequest } = require('./routes/storage.js');
@@ -2083,47 +2162,80 @@ const _handleAi = async (req, res) => {
   };
 
 try {
+  let {pathname: p} = url.parse(req.url, true);
+  
   if (req.method === 'OPTIONS') {
     _setCorsHeaders(res);
     res.end();
   } else if (req.method === 'POST' && req.headers['authorization'] === 'Password ' + config.devPassword) {
-    _setCorsHeaders(res);
-    
-    const b = await new Promise((accept, reject) => {
-      const bs = [];
-      req.on('data', d => {
-        bs.push(d);
+    if (p === '/') {
+      _setCorsHeaders(res);
+      
+      const b = await new Promise((accept, reject) => {
+        const bs = [];
+        req.on('data', d => {
+          bs.push(d);
+        });
+        req.on('end', () => {
+          const b = Buffer.concat(bs);
+          bs.length = 0;
+          accept(b);
+        });
+        req.on('error', reject);
       });
-      req.on('end', () => {
-        const b = Buffer.concat(bs);
-        bs.length = 0;
-        accept(b);
+      const s = b.toString('utf8');
+      const o = JSON.parse(s);
+      
+      console.log('got o', o);
+
+      const gptResponse = await openai.complete({
+        engine: 'davinci',
+        // stream: false,
+        prompt: o.prompt, // 'this is a test',
+        maxTokens: o.maxTokens, // 5,
+        temperature: o.temperature, // 0.9,
+        topP: o.topP, // 1,
+        presencePenalty: o.presencePenalty, // 0,
+        frequencyPenalty: o.frequencyPenalty, // 0,
+        bestOf: o.bestOf, // 1,
+        n: o.n, // 1,
+        stop: o.stop, // ['\n']
       });
-      req.on('error', reject);
-    });
-    const s = b.toString('utf8');
-    const o = JSON.parse(s);
-    
-    console.log('got o', o);
 
-    const gptResponse = await openai.complete({
-      engine: 'davinci',
-      // stream: false,
-      prompt: o.prompt, // 'this is a test',
-      maxTokens: o.maxTokens, // 5,
-      temperature: o.temperature, // 0.9,
-      topP: o.topP, // 1,
-      presencePenalty: o.presencePenalty, // 0,
-      frequencyPenalty: o.frequencyPenalty, // 0,
-      bestOf: o.bestOf, // 1,
-      n: o.n, // 1,
-      stop: o.stop, // ['\n']
-    });
+      console.log('got response');
+      console.log(gptResponse.data);
 
-    console.log('got response');
-    console.log(gptResponse.data);
+      res.end(JSON.stringify(gptResponse.data));
+    } else if (p === '/code') {
+      _setCorsHeaders(res);
 
-    res.end(JSON.stringify(gptResponse.data));
+      const aiPrefix = await getAiPrefix();
+
+      const p = decodeURIComponent(o.query.p);
+      const proxyRes = await _openAiCodex(aiPrefix + p + ' */\n', '\n/* Command: ');
+      if (proxyRes.statusCode >= 200 && proxyRes.statusCode < 300) {
+        for (const key in proxyRes.headers) {
+          const value = proxyRes.headers[key];
+          res.setHeader(key, value);
+        }
+        // console.log('render');
+        proxyRes.pipe(res);
+        proxyRes.on('data', d => {
+          console.log('got data', d.toString('utf8'));
+        });
+      } else {
+        proxyRes.setEncoding('utf8');
+        proxyRes.on('data', s => {
+          console.log(s);
+        });
+        res.setHeader('Content-Type', 'text/event-stream');
+        res.end('data: [DONE]');
+      }
+    } else {
+      _respond(404, JSON.stringify({
+        error: 'path not found',
+      }));
+    }
   } else {
     _respond(403, JSON.stringify({
       error: 'invalid password',
