9c39b2394f0eaccb7df2d937c5f2d38063c81259
Empec <rob@raspberrydreamlabs.com>
2022-06-08 14:04:39 +0100

    created compose file and conditions to connect to local scylladb

--- aws.js
+++ aws.js
@@ -12,6 +12,13 @@ const awsConfig = new AWS.Config({
     }),
     region: 'us-west-1',
 });
+
+if (config.dbLocal="TRUE") {
+awsConfig.update({
+  endpoint: "http://db:8000"
+});
+}
+
 const s3 = new AWS.S3(awsConfig);
 
 const ddb = new AWS.DynamoDB({
@@ -24,78 +31,8 @@ const ddbd = new AWS.DynamoDB.DocumentClient({
   apiVersion: '2012-08-10',
 });
 
-const defaultDynamoTable = 'sidechain-cache';
-
-async function getDynamoItem(id, TableName) {
-  const params = {
-    TableName,
-    Key: {
-      id,
-    },
-  };
-
-  try {
-    return await ddbd.get(params).promise();
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-
-async function putDynamoItem(id, data, TableName) {
-  const params = {
-    TableName,
-    Item: {
-      ...data,
-      id,
-    },
-  };
-
-  try {
-    return ddbd.put(params).promise();
-  } catch (e) {
-    console.error(e);
-    return false;
-  }
-}
-
-async function getDynamoAllItems(TableName = defaultDynamoTable) {
-  const params = {
-    TableName,
-  };
-
-  try {
-    const o = await ddbd.scan(params).promise();
-    const items = (o && o.Items) || [];
-    return items;
-  } catch (e) {
-    console.error(e);
-    return null;
-  }
-}
-
-function uploadFromStream(bucket, key, type) {
-  const pass = new stream.PassThrough();
-  const params = {Bucket: bucket, Key: key, Body: pass, ACL: 'public-read'};
-  if (type) {
-    params['ContentType'] = type;
-  }
-  s3.upload(params, function(err, data) {
-    console.log('emit done', !!err, !!data);
-    if (err) {
-      pass.emit('error', err);
-    } else {
-      pass.emit('done', data);
-    }
-  });
-  return pass;
-}
-
 module.exports = {
+  s3,
   ddb,
   ddbd,
-  getDynamoItem,
-  putDynamoItem,
-  getDynamoAllItems,
-  uploadFromStream,
 }
